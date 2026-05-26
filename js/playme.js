0aa474e96bab81d0ca316f9bbf8ac9fd0cdc7da2
Xoleric <xoleric@xoleric.uz>
2026-05-27 02:26:57 +0500

    Add 12th video to playlist

--- js/playme.js
+++ js/playme.js
@@ -5,13 +5,14 @@ const DEFAULT_VIDEOS = [
   { file: RAW + '/2_5235775282278869717.mp4', title: 'Video 2' },
   { file: RAW + '/2_5237973231792597901.mp4', title: 'Video 3' },
   { file: RAW + '/2_5282749129141818157.mp4', title: 'Video 4' },
-  { file: RAW + '/2_5373350012551988338.mp4', title: 'Video 5' },
-  { file: RAW + '/2_5447322629427989855.mp4', title: 'Video 6' },
-  { file: RAW + '/2_5452074624193953955.mp4', title: 'Video 7' },
-  { file: RAW + '/2_5458622658318987050.mp4', title: 'Video 8' },
-  { file: RAW + '/2_5458751034891467046.mp4', title: 'Video 9' },
-  { file: RAW + '/2_5458751034891467056.mp4', title: 'Video 10' },
-  { file: RAW + '/2_5462948497839910298.mp4', title: 'Video 11' },
+  { file: RAW + '/2_5287781263149656497.mp4', title: 'Video 5' },
+  { file: RAW + '/2_5373350012551988338.mp4', title: 'Video 6' },
+  { file: RAW + '/2_5447322629427989855.mp4', title: 'Video 7' },
+  { file: RAW + '/2_5452074624193953955.mp4', title: 'Video 8' },
+  { file: RAW + '/2_5458622658318987050.mp4', title: 'Video 9' },
+  { file: RAW + '/2_5458751034891467046.mp4', title: 'Video 10' },
+  { file: RAW + '/2_5458751034891467056.mp4', title: 'Video 11' },
+  { file: RAW + '/2_5462948497839910298.mp4', title: 'Video 12' },
 ];
 
 // Fetch + Blob proxy for correct MIME type
